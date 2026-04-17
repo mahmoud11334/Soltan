7cafae4326ad440938b4175ed6a5fd68a5a0684f
mahmoud11334 <7oka12345678910@gmail.com>
2026-04-17 19:02:03 +0200

    first commit

--- run.bat
+++
@@ -1,8 +0,0 @@
-E:
-cd E:\Data\VS code\test\Soltan
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/mahmoud11334/Soltan.git
-git push -u origin main
